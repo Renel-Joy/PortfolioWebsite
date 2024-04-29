49b9db6a670b9aee566d3a0abbbf88ac4b6eefd2
Renel-Joy <159543239+Renel-Joy@users.noreply.github.com>
2024-04-29 22:19:45 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,21 @@
+
+   <footer>
+
+      <div class="row">
+
+         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer> 
+
+ 
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="<?php echo base_url ?>profile_asset/js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="<?php echo base_url ?>profile_asset/js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="<?php echo base_url ?>profile_asset/js/jquery.flexslider.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/waypoints.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/jquery.fittext.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/magnific-popup.js"></script>
+   <script src="<?php echo base_url ?>profile_asset/js/init.js"></script>
